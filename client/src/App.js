0708964c41395ea44baea809492e29abcddf151e
ifaxec <xecfahim.ahmed@gmail.com>
2023-05-25 00:02:37 +0100

    Check REST api and client connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
+import React, { useState, useEffect } from 'react';
 
 
-function App() {
+const App = () => {
+  const [courses, setCourses] = useState([]);
+
+  const fetchCourses = async () => {
+    try {
+      const URL = 'http://localhost:5000/api/courses';
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCourses(data);
+    } catch (error) {
+      console.error('Error fetching course', error);
+    }
+  }
+
+  useEffect(() => {
+    try {
+      const courses = fetchCourses();
+      console.log("🚀 ~ file: App.js:20 ~ useEffect ~ courses:", courses)
+    } catch (error) {
+      console.error('Error with useEffect hook', error);
+    }
+  }, []);
+
+
   return (
     <div>
+      <h1>List of Courses</h1>
+      <ul>
+        {courses.map(course =>
+          <li key={course.id}>{course.title}</li>
+        )}
+      </ul>
 
     </div>
   );
 }
 
 export default App;
+
+
+
 
 
 
