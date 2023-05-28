c35d4340eb406f3d2b9aa3a8dc2cf04f06a4864e
ifaxec <xecfahim.ahmed@gmail.com>
2023-05-29 00:03:14 +0100

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 const UpdateCourse = () => {
-    return (
+    const [updateCourse, setUpdateCourse] = useState({
 
+
+    });
+    const navigate = useNavigate();
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate('/')
+    }
+
+    return (
 
         <div className="wrap">
             <h2>Update Course</h2>
@@ -25,10 +39,10 @@ const UpdateCourse = () => {
                         <textarea id="materialsNeeded" name="materialsNeeded">* 1/2 x 3/4 inch parting strip&#13;&#13;* 1 x 2 common pine&#13;&#13;* 1 x 4 common pine&#13;&#13;* 1 x 10 common pine&#13;&#13;* 1/4 inch thick lauan plywood&#13;&#13;* Finishing Nails&#13;&#13;* Sandpaper&#13;&#13;* Wood Glue&#13;&#13;* Wood Filler&#13;&#13;* Minwax Oil Based Polyurethane</textarea>
                     </div>
                 </div>
-                <button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button className="button" type="submit">Update Course</button>
+                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
         </div>
-
 
     );
 }
