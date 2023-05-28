f680fd2c662ea1fed76608eaf99e693e5d1731bd
ifaxec <xecfahim.ahmed@gmail.com>
2023-05-28 23:08:47 +0100

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,53 +1,35 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 const CreateCourse = () => {
 
     const navigate = useNavigate();
-    const [courseTitle, setCourseTitle] = useState('');
-    const [courseDescription, setCourseDescription] = useState('');
-    const [courseEstimatedTime, setCourseEstimatedTime] = useState('');
-    const [courseMaterialsNeeded, setCourseMaterialsNeeded] = useState('');
+    const [courseData, setCourseData] = useState({
+        title: '',
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: ''
+    });
 
-    const handleCourseTitleChange = (event) => {
-        console.log(event.target.value);
-        setCourseTitle(event.target.value);
+    //When user updated input field 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setCourseData((prevData) => ({ ...prevData, [name]: value }))
     }
 
-    const handleCourseDescriptionChange = (event) => {
-        console.log(event.target.value)
-        setCourseDescription(event.target.value);
-    }
-
-    const handleCourseEstimateTimeChange = (event) => {
-        setCourseEstimatedTime(event.target.value);
-    }
-
-    const handleCourseMaterialsNeededChange = (event) => {
-        setCourseMaterialsNeeded(event.target.value);
-    }
-
+    //When user submits the form 
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const courseData = {
-                courseTitle,
-                courseDescription,
-                courseEstimatedTime,
-                courseMaterialsNeeded
-            }
-
-            const URL = 'http://localhost:5000/api/courses'
+            const URL = 'http://localhost:5000/api/courses';
             const response = await fetch(URL, {
                 method: 'POST',
                 header: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json; charset=UTF-8',
                 },
                 body: JSON.stringify(courseData),
             })
+            console.log("🚀 ~ response:", response);
 
             if (response.ok) {
                 navigate('/')
@@ -60,6 +42,11 @@ const CreateCourse = () => {
         }
     }
 
+    //When user clicks the cancel button
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate('/');
+    }
 
     return (
 
@@ -72,28 +59,30 @@ const CreateCourse = () => {
                     <li>Please provide a value for "Description"</li>
                 </ul>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="main--flex">
                     <div>
                         <label htmlFor="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value="" />
+                        <input id="courseTitle" name="courseTitle" type="text" value={courseData.title} onChange={handleChange} />
+                        {/* //FIXME - Course Title is readonly */}
 
                         <p>By Joe Smith</p>
 
                         <label htmlFor="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription"></textarea>
+                        <textarea id="courseDescription" name="courseDescription" value={courseData.description} onChange={handleChange}></textarea>
                     </div>
                     <div>
                         <label htmlFor="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value="" />
+                        <input id="estimatedTime" name="estimatedTime" type="text" value={courseData.estimatedTime} onChange={handleChange} />
 
                         <label htmlFor="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+                        <textarea id="materialsNeeded" name="materialsNeeded" value={courseData.materialsNeeded} onChange={handleChange} ></textarea>
                     </div>
                 </div>
-                <button className="button" type="submit">Create Course</button><button className="button button-secondary" onClick="event.preventDefault(); location.NavLink to ='/';">Cancel</button>
+                <button className="button" type="submit" onClick={handleSubmit}>Create Course</button>
+                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
-        </div>
+        </div >
 
     );
 
